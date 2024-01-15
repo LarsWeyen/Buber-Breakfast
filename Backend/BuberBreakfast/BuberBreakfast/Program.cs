e82040d139f6d91c4989b2b519c9b62cb83499c2
LarsWeyen <97236276+LarsWeyen@users.noreply.github.com>
2024-01-15 16:11:20 +0100

    build(backend): creating .net web-api

---
+++ Backend/BuberBreakfast/BuberBreakfast/Program.cs
@@ -0,0 +1,30 @@
+using BuberBreakfast.Services.Breakfasts;
+
+var builder = WebApplication.CreateBuilder(args);
+{
+builder.Services.AddControllers();
+builder.Services.AddScoped<IBreakfastService,BreakfastService>();
+}
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/error");
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+}
+
