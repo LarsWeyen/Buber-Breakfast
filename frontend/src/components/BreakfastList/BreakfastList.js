38fc9f5d041c7dd8a199ff0cf509caade601f3f9
LarsWeyen <97236276+LarsWeyen@users.noreply.github.com>
2024-01-16 14:22:22 +0100

    feat(frontend): added breakfastlist functionality and styling

--- frontend/src/components/BreakfastList/BreakfastList.js
+++ frontend/src/components/BreakfastList/BreakfastList.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import BreakfastItem from '../BreakfastItem/BreakfastItem';
+
 
 export const BreakfastList = () => {
-  return (
-    <div>BreakfastList</div>
+  const [breakfasts, setBreakfasts] = useState(null);
+  const getBreakfasts = async () => {
+    axios.get('http://localhost:5090/Breakfast')
+    .then(res =>{
+      setBreakfasts(res.data)
+    })
+    .catch(err =>{
+      console.log(err)
+    })
+  }
+  useEffect(()=>{
+    getBreakfasts()
+  },[])
+
+  return (  
+    <div className='breakfast-list-container'>
+      {breakfasts ? (
+        breakfasts.map((breakfast,index)=>{
+          return(
+            <BreakfastItem breakfast={breakfast} key={index}/>
+          )
+        })
+      ):(
+        <span>No breakfast created yet</span>
+      )}
+      
+    
+    </div>
   )
 }
