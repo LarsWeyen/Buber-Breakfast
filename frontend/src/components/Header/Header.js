4f12e225009f9d5e776b86a7e1af4e70653aea19
LarsWeyen <97236276+LarsWeyen@users.noreply.github.com>
2024-01-17 15:12:17 +0100

    refactor(frontend): moved header into a separate component

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div className="header">
+    <h1>Buber <span className='blue-highlighter'>Breakfast</span></h1>
+    <hr />
+  </div>
+  )
+}
